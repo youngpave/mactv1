edda6342ca6e26618f5085444ffcf57d06cd2009
youngpave <dabdulbari8@gmail.com>
2026-03-27 19:58:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,8 @@
-self.addEventListener('install', (e) => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
 self.addEventListener('fetch', (event) => {
-    const url = event.request.url;
-    // IPTV linklerini ve portlarını (8080 vb.) yakala
-    if (url.includes('.m3u8') || url.includes('.ts') || url.includes(':')) {
+    if (event.request.url.startsWith('http://')) {
         event.respondWith(
-            fetch(event.request, {
-                mode: 'cors',
-                referrerPolicy: 'no-referrer'
-            }).then(response => {
-                // Tarayıcıyı kandıran başlıklar
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set('Access-Control-Allow-Origin', '*');
-                newHeaders.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-                newHeaders.set('Access-Control-Allow-Headers', '*');
-
-                return new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders
-                });
+            fetch(event.request, { 
+                mode: 'no-cors' // Tarayıcıyı HTTP konusunda zorlar
             }).catch(() => fetch(event.request))
         );
     }
